ce7991df1bd8b89f96283ae47e49576d3d5f11dc
Jo Bovy <bovy@ias.edu>
2011-09-15 16:51:02 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup #, Extension
+import os, os.path
+import re
+
+longDescription= ""
+
+
+setup(name='isodist',
+      version='1.',
+      description='spectro-photometric distances to stars
+      author='Jo Bovy',
+      author_email='bovy@ias.edu',
+      license='New BSD',
+      long_description=longDescription,
+      url='http://code.google.com/p/isodist/',
+      package_dir = {'isodist/': ''},
+      packages=['isodist'],
+#      dependency_links = ['https://github.com/dfm/MarkovPy/tarball/master#egg=MarkovPy'],
+      install_requires=['numpy','scipy']
+      )
