43411780b058556982659b4ca16247feeed768a6
Jo Bovy <bovy@ias.edu>
2012-07-23 14:53:15 +0200

    Add Basti to top-level

--- isodist/__init__.py
+++ isodist/__init__.py
@@ -2,3 +2,4 @@ from _isodist import *
 from Isochrone import FEH2Z, Z2FEH
 from PadovaIsochrone import PadovaIsochrone, padovaTypes
 from AnIsochrone import AnIsochrone
+from BastiIsochrone import BastiIsochrone
