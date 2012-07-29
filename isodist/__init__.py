099ca5bd8183d41f80620f611e9d93936a072c1b
Jo Bovy <bovy@ias.edu>
2012-07-29 13:53:09 +0200

    add Dartmouth reader

--- isodist/__init__.py
+++ isodist/__init__.py
@@ -3,3 +3,4 @@ from Isochrone import FEH2Z, Z2FEH
 from PadovaIsochrone import PadovaIsochrone, padovaTypes
 from AnIsochrone import AnIsochrone
 from BastiIsochrone import BastiIsochrone
+from DartmouthIsochrone import DartmouthIsochrone
